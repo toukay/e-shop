80cfacc017e5513b4797cf557fcff35910d3f2dd
toukay <35756997+toukay@users.noreply.github.com>
2024-07-15 17:24:04 +0200

    Setup models attributes.

--- src/models.py
+++ src/models.py
@@ -1,17 +1,44 @@
-from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, Mapped, mapped_column
+from sqlalchemy.orm import (
+    DeclarativeBase, 
+    MappedAsDataclass, 
+    Mapped,
+    mapped_column,
+    relationship
+)
+from sqlalchemy import (
+    ForeignKey,
+    String,
+    Integer,
+    DECIMAL
+)
 import pydantic
+from decimal import Decimal
 
 class Base(DeclarativeBase, MappedAsDataclass, dataclass_callable=pydantic.dataclasses.dataclass):
     pass
 
 class Product(Base):
-    pass
+    __tablename__ = "Product"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String)
+    description: Mapped[str] = mapped_column(String)  
+    price: Mapped[Decimal] = mapped_column(DECIMAL(precision=10, scale=2))
 
 class ProductType(Base):
-    pass
+    __tablename__ = "Product_Type"
+    
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String)
 
 class Brand(Base):
-    pass
+    __tablename__ = "Brand"
+    
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String)
 
 class Category(Base):
-    pass
+    __tablename__ = "Category"
+    
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String)
