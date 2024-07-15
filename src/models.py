1fee90870d56e53da787b57a7fcd2b3fc4a7f081
toukay <35756997+toukay@users.noreply.github.com>
2024-07-15 18:05:19 +0200

    Setup the relationships between the models.

--- src/models.py
+++ src/models.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import (
     relationship
 )
 from sqlalchemy import (
+    Table,
+    Column,
     ForeignKey,
     String,
     Integer,
@@ -17,28 +19,67 @@ from decimal import Decimal
 class Base(DeclarativeBase, MappedAsDataclass, dataclass_callable=pydantic.dataclasses.dataclass):
     pass
 
+
+product_category_association = Table(
+    "product_category",
+    Base.metadata,
+    Column('product_id', ForeignKey('product.id'), primary_key=True),
+    Column('category_id', ForeignKey('category.id'), primary_key=True),
+)
+
+
 class Product(Base):
-    __tablename__ = "Product"
+    __tablename__ = "product"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String)
     description: Mapped[str] = mapped_column(String)  
     price: Mapped[Decimal] = mapped_column(DECIMAL(precision=10, scale=2))
 
-class ProductType(Base):
-    __tablename__ = "Product_Type"
+    brand_id: Mapped[int] = mapped_column(ForeignKey("brand.id"))
+    product_type_id: Mapped[int] = mapped_column(ForeignKey("product_type.id"))
     
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(String)
+    brand: Mapped['Brand'] = relationship('Brand', back_populates="products")
+    product_type: Mapped['ProductType'] = relationship('ProductType', back_populates="products")
+    categories: Mapped[list['Category']] = relationship(
+        'Category', 
+        secondary=product_category_association, 
+        back_populates="products", 
+        default_factory=lambda: []
+    )
+
 
 class Brand(Base):
-    __tablename__ = "Brand"
+    __tablename__ = "brand"
     
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String)
 
-class Category(Base):
-    __tablename__ = "Category"
+    products: Mapped[list['Product']] = relationship(
+        'Product', back_populates="brand", default_factory=lambda: []
+    )
+
+
+class ProductType(Base):
+    __tablename__ = "product_type"
     
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String)
+
+    products: Mapped[list['Product']] = relationship(
+        'Product', back_populates="product_type", default_factory=lambda: []
+    )
+
+
+class Category(Base):
+    __tablename__ = "category"
+    
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String)
+
+    products: Mapped[list['Product']] = relationship(
+        'Product',
+        secondary=product_category_association,
+        back_populates="categories", 
+        default_factory=lambda: []
+    )
