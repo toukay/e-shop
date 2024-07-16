63c8920acc10709640fc593b04a285ba9f38e3f9
toukay <35756997+toukay@users.noreply.github.com>
2024-07-16 15:29:48 +0200

    Change to local file db instead of in memory.

--- src/database.py
+++ src/database.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-DB_URL = "sqlite:///:memory:"
+DB_URL = "sqlite:///eshop.db"
 
 engine = create_engine(DB_URL, connect_args={"check_same_thread": False}, echo=True)
 SessionLocal  = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
-def init_db():
+def create_all():
     Base.metadata.create_all(bind=engine)
 
 def generate_session():
