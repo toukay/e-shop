a95b8e148dcb81f0ddf3dce4d36126a91f9ea438
toukay <35756997+toukay@users.noreply.github.com>
2024-07-16 15:57:33 +0200

    Update Sqlite database file url.

--- src/database.py
+++ src/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
+import os
 
-DB_URL = "sqlite:///eshop.db"
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+DB_URL = f"sqlite:///{os.path.join(base_dir, 'eshop.db')}"
 
 engine = create_engine(DB_URL, connect_args={"check_same_thread": False}, echo=True)
 SessionLocal  = sessionmaker(autoflush=False, autocommit=False, bind=engine)
