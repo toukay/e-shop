7251d56a0de4e7220230ec1cf723ddd5ce7dd59f
toukay <35756997+toukay@users.noreply.github.com>
2024-07-19 17:00:53 +0200

    Setup core module with models and services.

--- src/web/src/app/core/models/product.ts
+++ src/web/src/app/core/models/product.ts
@@ -1,2 +1,13 @@
+import { Brand } from "./brand";
+import { Category } from "./category";
+import { ProductType } from "./product-type";
+
 export interface Product {
+    id: number,
+    name: string,
+    description: string,
+    price: number,
+    brand: Brand,
+    productType: ProductType,
+    categories: Category[],
 }
--- src/web/src/app/core/services/products.service.ts
+++ src/web/src/app/core/services/products.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  url = "http://localhost:5000/api/";
+  url = "http://localhost:5000/api";
 
   constructor() { }
 
+  async getAllProducts() : Promise<Product[]> {
+    const data = await fetch(`${this.url}/products`);
+    return await data.json() ?? [];
+  }
+
+  async getProductById(id: Number) : Promise<Product | undefined> {
+    const data = await fetch(`${this.url}/products/${id}`);
+    return await data.json() ?? {};
+  }
+
+  // submitApplication(firstName: string, lastName: string, email: string) {
+  //   console.log(firstName, lastName, email);
+  // }
 }
