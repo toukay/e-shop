4df44bd5e55203de9e6752334bb1320c69966a89
toukay <35756997+toukay@users.noreply.github.com>
2024-07-19 17:05:24 +0200

    Setup Home module and its components with routing.

--- src/web/src/app/app.component.ts
+++ src/web/src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './modules/home/pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'web';
+  title = 'products';
 }
---
+++ src/web/src/app/modules/home/components/product-listing/product-listing.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-listing',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      product-listing works!
+    </p>
+  `,
+  styleUrl: './product-listing.component.css'
+})
+export class ProductListingComponent {
+
+}
